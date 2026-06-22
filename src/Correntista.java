d9601c7d15ce2285beecff06d023a87b0b1147da
Lucas Djalma <lucasdjalma@gmail.com>
2026-06-22 18:45:05 -0300

    Add Banco and Correntista classes with basic functionality

---
+++ src/Correntista.java
@@ -0,0 +1,27 @@
+public class Correntista {
+
+    private int Codigo;
+    private String nome;
+
+    public Correntista(int Codigo, String nome) {
+        this.Codigo = Codigo;
+        this.nome = nome;
+    }
+
+    // Getters and setters
+    public int getCodigo() {
+        return Codigo;
+    }
+
+    public void setCodigo(int Codigo) {
+        this.Codigo = Codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
