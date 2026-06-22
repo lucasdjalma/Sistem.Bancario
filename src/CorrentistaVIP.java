d06ae25d7f4aa8f03ff4c484f821a6676cdffca0
Lucas Djalma <lucasdjalma@gmail.com>
2026-06-22 19:39:57 -0300

    Refactor Correntista constructor to use static method for unique code generation

---
+++ src/CorrentistaVIP.java
@@ -0,0 +1,40 @@
+public class CorrentistaVIP {
+
+    private static int proxCodigo = 1;
+    // Variável estática para gerar códigos únicos
+
+    private int Codigo;
+    private String nome;
+    private double limiteCredito;
+
+    public CorrentistaVIP(String nome, double limiteCredito) {
+        this.nome = nome;
+        this.limiteCredito = limiteCredito;
+        this.Codigo = getProxCodigo();
+    }
+
+    public static int getProxCodigo() {
+        return proxCodigo++;
+    }
+
+    // Getters and setters
+    public int getCodigo() {
+        return Codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getLimiteCredito() {
+        return limiteCredito;
+    }
+
+    public void setLimiteCredito(double limiteCredito) {
+        this.limiteCredito = limiteCredito;
+    }
+}
