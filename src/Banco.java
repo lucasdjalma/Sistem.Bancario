3ca1213b5129286ec56a4828d710ca29010f6afb
Lucas Djalma <lucasdjalma@gmail.com>
2026-06-22 20:08:52 -0300

    Refactor Banco class to utilize ArrayList for client management and enhance output formatting

--- src/Banco.java
+++ src/Banco.java
@@ -1,20 +1,27 @@
+import java.util.ArrayList;
+
+import java.util.List;
 public class Banco {
 
     public static void main(String[] args) {
+        ArrayList<Correntista> listaDeClientes = new ArrayList<Correntista>();
 
-        Correntista c1 = new Correntista( "João da Silva");
-        Correntista c2 = new Correntista( "Maria Oliveira");
-        Correntista c3 = new Correntista( "Carlos Souza");
-        Correntista c4 = new Correntista( "Ana Santos");
+        CorrentistaFisico c1 = new CorrentistaFisico( "João da Silva");
+        listaDeClientes.add(c1);
+        CorrentistaFisico c2 = new CorrentistaFisico( "Maria Oliveira");
+        listaDeClientes.add(c2);
+        CorrentistaFisico c3 = new CorrentistaFisico( "Carlos Souza");
+        listaDeClientes.add(c3);
+        CorrentistaFisico c4 = new CorrentistaFisico( "Ana Santos");
+        listaDeClientes.add(c4);
 
-        System.out.println("Código: " + c1.getCodigo());
-        System.out.println("Nome: " + c1.getNome());
-        System.out.println("Código: " + c2.getCodigo());
-        System.out.println("Nome: " + c2.getNome());
-        System.out.println("Código: " + c3.getCodigo());
-        System.out.println("Nome: " + c3.getNome());
-        System.out.println("Código: " + c4.getCodigo());
-        System.out.println("Nome: " + c4.getNome());
+        CorrentistaJuridico cJuridico = new CorrentistaJuridico("Empresa XYZ", "Pedro Almeida");
+        listaDeClientes.add(cJuridico);
+
+        for (Correntista cliente : listaDeClientes) {
+            System.out.println("Código: " + cliente.getCodigo());
+            System.out.println("Nome: " + cliente.getNome());
+        }
     }
 
 
